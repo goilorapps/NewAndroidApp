1c34b6d2998b70f99cfa5066ff5145587c08fc45
Loren <loren.djkiller@gmail.com>
2019-02-10 02:48:36 +0100

    Added Metodos class Created onClick Method Created second Screen

--- app/src/main/java/com/example/goilor/MainActivity.java
+++ app/src/main/java/com/example/goilor/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.goilor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Prueba
+
     }
+
+    public void onClick(View vista){
+        Button boton = (Button) vista;
+
+        if(boton.getId() == R.id.btn_Inicio){
+            Intent intento = new Intent(this,Pantalla2.class);
+            startActivity(intento);
+        }
+    }
+
+
+
+
 }
